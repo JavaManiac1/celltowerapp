ee4d7103d57adf8c6ad299570991315e8f4cc421
Damian <damian_joniec@interia.pl>
2019-11-09 22:35:41 +0100

    Initialize commit

---
+++ src/main/java/com/celltowerapp/CelltowerwebappApplication.java
@@ -0,0 +1,13 @@
+package com.celltowerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelltowerwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CelltowerwebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/celltowerapp/CelltowerwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celltowerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelltowerwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
